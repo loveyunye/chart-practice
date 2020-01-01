8d1a2ac4c82606a2e56b569eeed3afcd65c0f533
xujiabin <xujb@citydo.com.cn>
2020-01-01 08:45:25 +0800

    组件渲染功能完善

--- src/app/Main.vue
+++ src/app/Main.vue
@@ -1,42 +1,22 @@
 <template>
-  <div class="main-wrapper"></div>
+  <div class="main-wrapper">
+    <ChartWrapper />
+  </div>
 </template>
 <script>
+import ChartWrapper from '../components/ChartWrapper';
 export default {
   name: 'main-index',
-  methods: {
-    async initCharts() {
-      const { default: Chart } = await import('../charts/PiePercentage');
-      const chart = new Chart(this.$el);
-      chart.initChart();
-      // chart.setData(0.8);
-      chart.setData([
-        {
-          name: '12-21',
-          value: 123,
-        },
-        {
-          name: '12-22',
-          value: 123,
-        },
-        {
-          name: '12-23',
-          value: 123,
-        },
-        {
-          name: '12-25',
-          value: 123,
-        },
-      ]);
-    },
-  },
-  mounted() {
-    this.initCharts();
+  components: {
+    ChartWrapper,
   },
 };
 </script>
 <style lang="less" scoped>
 .main-wrapper {
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
--- src/store/modules/chart.js
+++ src/store/modules/chart.js
@@ -1,16 +1,19 @@
-import cloneDeep from 'lodash.clonedeep';
+import { cloneDeep } from 'lodash';
 const screen = {
   namespaced: true,
   state: {
     chartClass: null,
     config: null,
     defaultOptions: null,
+    defaultData: null,
     key: '',
   },
   mutations: {
     setCurrentChart(state, current) {
-      state.chartClass = cloneDeep(current.chart);
+      console.log(current);
+      state.chartClass = current.chart;
       state.defaultOptions = cloneDeep(current.chart.defaultOptions || {});
+      state.defaultData = cloneDeep(current.config.data);
       state.config = cloneDeep(current.config);
       state.key = current.key;
     },
