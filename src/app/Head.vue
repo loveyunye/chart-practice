7fcefb6a35b13ddc6d440f3158c9cc6144d47591
loveyunye <13755867654@163.com>
2020-05-28 16:49:27 +0800

    新增链接

--- src/app/Head.vue
+++ src/app/Head.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="head-wrapper">
     <div class="logo">CHART PARCTICE</div>
+    <div class="link">
+      <a href="/game.html">扫雷</a>
+      <a href="/game1.html">贪吃蛇</a>
+    </div>
   </div>
 </template>
 <style lang="less" scoped>
@@ -19,6 +23,12 @@
     background-image: linear-gradient(0deg, #7349be 0%, #e57d84 100%);
     background-clip: text;
     -webkit-text-fill-color: transparent;
+  }
+  .link {
+    a {
+      margin-left: 10px;
+      color: #ffffff;
+    }
   }
 }
 </style>
